73784b5f8bc13a80a6f732e2b122919e1dd069db
tnah-hcmus <tnah.work@gmail.com>
2019-11-11 08:47:22 +0700

    Add login page and js toggle-function for log-form

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
-  
+  // Toggle dropdown submenu
    $(document).ready(function(){
       $('.dropdown-submenu a.drop').click(function(e){
          $(this).next('ul').toggle();
@@ -46,5 +46,26 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+  
+  // Toggle signup, reset password in logged form
+  function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(document).ready(function(){
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
 
 })(jQuery); // End of use strict
