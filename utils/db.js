2edc1e8f03bd30fd7fc9e48a4b8dca73e5556d13
gialoinguyen <gialoinguyen081299@gmail.com>
2020-01-07 03:10:30 +0700

    ! Fix login bidder err

--- utils/db.js
+++ utils/db.js
@@ -2,16 +2,16 @@ const mysql = require('mysql');
 const util = require('util');
 
 const pool = mysql.createPool({
-  connectionLimit: 50,
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'auction'
+    connectionLimit: 50,
+    host: 'localhost',
+    port: 8966,
+    user: 'root',
+    password: '',
+    database: 'auction'
 });
 
 const mysql_query = util.promisify(pool.query).bind(pool);
 
 module.exports = {
-  load: sql => mysql_query(sql)
+    load: sql => mysql_query(sql)
 };
