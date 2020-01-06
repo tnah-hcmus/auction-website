f7401d89f054152254c85d0986c82ad651d49c09
tnah-hcmus <tnah.work@gmail.com>
2020-01-07 02:39:24 +0700

    deploy db.js

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,10 @@ const util = require('util');
 
 const pool = mysql.createPool({
   connectionLimit: 50,
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'auction'
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'ba0d6bf79f6ab3',
+  password: '5e9fd300',
+  database: 'heroku_db602d5617b4c0c'
 });
 
 const mysql_query = util.promisify(pool.query).bind(pool);
